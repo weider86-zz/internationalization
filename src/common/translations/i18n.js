fb6b51d1ae5fbc90bbf6bf7d896b736b57c6de97
Weider Lima <weider.lima@mercadolivre.com>
2020-08-18 17:23:24 -0300

    Add useless dependency config

--- src/common/translations/i18n.js
+++ src/common/translations/i18n.js
@@ -1,28 +1,11 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
 
-import Backend from "i18next-xhr-backend";
-
-i18n
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
-  // connect with React
-  .use(initReactI18next)
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    debug: true,
-
-    lng: "en",
-    fallbackLng: "en",
-    whitelist: ["en", "pt"],
-
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-
-    backend: {
-      loadPath: "/translations/locales/{{lng}}/{{ns}}.json",
-    },
-  });
+i18n.use(initReactI18next).init({
+  debug: true,
+  lng: 'en',
+  fallbackLng: 'en',
+  whitelist: ['en', 'pt-br', 'es'],
+});
 
 export default i18n;
