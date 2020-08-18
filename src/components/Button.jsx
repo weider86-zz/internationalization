2bf9ff04a9f00bd70b949c8a94cfa04e1ba141f0
Weider Lima <weider.lima@mercadolivre.com>
2020-08-18 17:58:19 -0300

    Add styled button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Button = styled.button`
+  margin: 20px;
+  padding: 0;
+  outline: none;
+  line-height: 0;
+  border-radius: 100%;
+  border-width: 3px;
+  border-style: solid;
+  border-color: ${(props) => (props.active ? "cadetBlue" : "white")};
+`;
