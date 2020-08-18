718d089120c8f6ac7e2657a21223426bf5f6fc59
Weider Lima <weider.lima@mercadolivre.com>
2020-08-18 15:14:51 -0300

    Setup i18n config

--- src/common/translations/i18n.js
+++ src/common/translations/i18n.js
@@ -1,7 +1,7 @@
-import i18n from 'i18next';
-import {initReactI18next} from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import Backend from 'i18next-xhr-backend';
+import Backend from "i18next-xhr-backend";
 
 i18n
   // learn more: https://github.com/i18next/i18next-xhr-backend
@@ -12,16 +12,16 @@ i18n
   .init({
     debug: true,
 
-    lng: 'en',
-    fallbackLng: 'en',
-    whitelist: ['en', 'pt'],
+    lng: "en",
+    fallbackLng: "en",
+    whitelist: ["en", "pt"],
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
 
     backend: {
-      loadPath: '/translations/locales/{{lng}}/{{ns}}.json',
+      loadPath: "/translations/locales/{{lng}}/{{ns}}.json",
     },
   });
 
