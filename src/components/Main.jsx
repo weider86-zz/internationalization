adb949667534f79137da23539323e7c356621f18
Weider Lima <weider.lima@mercadolivre.com>
2020-08-18 17:59:07 -0300

    Consume styled button component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Button } from "./Button";
 import uk_flag from "../assets/images/uk-flag.png";
 import brazil_flag from "../assets/images/brazil-flag.png";
 import spanish_flag from "../assets/images/spanish-flag.png";
@@ -11,15 +12,24 @@ export const Main = () => {
   return (
     <div>
       <h1>{translate("title", { project: "Internacionalization " })} app</h1>
-      <button onClick={() => i18n.changeLanguage("default_en")}>
+      <Button
+        active={i18n.language === "default_en"}
+        onClick={() => i18n.changeLanguage("default_en")}
+      >
         <img src={uk_flag} alt="English version" />
-      </button>
-      <button onClick={() => i18n.changeLanguage("default_pt_br")}>
+      </Button>
+      <Button
+        active={i18n.language === "default_pt_br"}
+        onClick={() => i18n.changeLanguage("default_pt_br")}
+      >
         <img src={brazil_flag} alt="Portuguese-Brazil version" />
-      </button>
-      <button onClick={() => i18n.changeLanguage("default_es")}>
+      </Button>
+      <Button
+        active={i18n.language === "default_es"}
+        onClick={() => i18n.changeLanguage("default_es")}
+      >
         <img src={spanish_flag} alt="Spanish version" />
-      </button>
+      </Button>
       <p>{translate("text")}</p>
       <p>
         {`${translate("repo_access")}: `}
