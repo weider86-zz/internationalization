509f8bdaa30ba492051dc32cefa6b447d6f10e7c
Weider Lima <weider.lima@mercadolivre.com>
2020-08-19 10:45:39 -0300

    Mock image files to jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
+const rootPath = '<rootDir>/src';
+
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/jestSetup.js"],
+  setupFilesAfterEnv: ['<rootDir>/jestSetup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif)$': `${rootPath}/fileMock.js`,
+  },
 };
