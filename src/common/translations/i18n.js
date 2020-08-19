57193b37cf1f99a06ba190843f7249f44d6ed0ed
Weider Lima <weider.lima@mercadolivre.com>
2020-08-19 16:26:41 -0300

    Set i18n config file

--- src/common/translations/i18n.js
+++ src/common/translations/i18n.js
@@ -1,11 +1,23 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18next from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import default_en_translation from './default-en.json';
+import default_pt_br_translation from './default-pt-br.json';
+import default_es_translation from './default-es.json';
 
-i18n.use(initReactI18next).init({
-  debug: true,
-  lng: "en",
-  fallbackLng: "en",
-  whitelist: ["en", "pt-br", "es"],
+i18next.use(initReactI18next).init({
+  lng: 'default_en',
+  fallbackLng: 'default_en',
+  resources: {
+    default_en: {
+      common: default_en_translation,
+    },
+    default_pt_br: {
+      common: default_pt_br_translation,
+    },
+    default_es: {
+      common: default_es_translation,
+    },
+  },
 });
 
-export default i18n;
+export default i18next;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "@babel/polyfill";
-import { App } from "./components/App";
-import ErrorBoundary from "./common/ErrorBoundary";
-import "./assets/styles/main.css";
-import { I18nextProvider } from "react-i18next";
-import i18next from "i18next";
-
-import default_en_translation from "./common/translations/default-en.json";
-import default_pt_br_translation from "./common/translations/default-pt-br.json";
-import default_es_translation from "./common/translations/default-es.json";
-
-i18next.init({
-  interpolation: { escapeValue: false },
-  lng: "default_en",
-  resources: {
-    default_en: {
-      common: default_en_translation,
-    },
-    default_pt_br: {
-      common: default_pt_br_translation,
-    },
-    default_es: {
-      common: default_es_translation,
-    },
-  },
-});
+import '@babel/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ErrorBoundary from './common/ErrorBoundary';
+import {App} from './components/App';
+import './common/translations/i18n';
+import './assets/styles/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <I18nextProvider i18n={i18next}>
-        <App />
-      </I18nextProvider>
+      <App />
     </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
